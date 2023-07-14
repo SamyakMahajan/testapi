91850fae2c6b087189f430bab1d0361ff906d0f5
SamyakMahajan <ketann682@gmail.com>
2023-07-14 15:49:50 +0530

    Response returns JSON output

--- HttpTrigger12/finalinit.py
+++ HttpTrigger12/finalinit.py
@@ -1,14 +1,23 @@
 import logging
 import azure.functions as func
+import json
+"""
+    what we will do:
+    1. give image url from azure storage to the API request
+    2. pass the URL to the predict function and return the tags 
+    3. return the tags in JSON format
+"""
+
 
 """
     Tasks done:
     1. Can Accept image_url as a request parameter and succesfuly return an OK response
+    2. Returns output as JSON type
 """
 
 """
     Tasks todo:
-    ultralytics library needs torch as a dependency but since it is a big size, we need to add torch using blob or as a dependency folder directly instead 
+    ultralytics library needs torch as a dependency but since it is a big size, we need to add torch using blob, ONNX file or as a dependency folder directly instead: currently trying out ONNX
 """
 
 
@@ -17,10 +26,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     image_url=req.params.get('image_url')
     logging.info('image_url: %s', image_url)
+
+    json_dict={
+        'image_url': image_url
+    }
+    json_object = json.dumps(json_dict, indent = 4) 
+    content_type='application/json'
     
-    return func.HttpResponse(
-            f"hi the image was recieved. Its URL is: {image_url} ",
-            status_code=200
-        )
+    return func.HttpResponse(body=json_object, status_code=200, mimetype=content_type)
+    
 
    
