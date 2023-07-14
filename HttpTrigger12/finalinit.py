48c394b151403d97a0f7ebfb564be73a80f110c6
SamyakMahajan <ketann682@gmail.com>
2023-07-14 15:11:18 +0530

    successfully takes in url in request parameter

--- HttpTrigger12/finalinit.py
+++ HttpTrigger12/finalinit.py
@@ -1,7 +1,15 @@
 import logging
 import azure.functions as func
 
+"""
+    Tasks done:
+    1. Can Accept image_url as a request parameter and succesfuly return an OK response
+"""
 
+"""
+    Tasks todo:
+    ultralytics library needs torch as a dependency but since it is a big size, we need to add torch using blob or as a dependency folder directly instead 
+"""
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -11,7 +19,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('image_url: %s', image_url)
     
     return func.HttpResponse(
-            f"hi {image_url} ",
+            f"hi the image was recieved. Its URL is: {image_url} ",
             status_code=200
         )
 
