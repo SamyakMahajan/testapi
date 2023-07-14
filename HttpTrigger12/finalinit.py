7f83d5551046f2a1c6d9104c18c45bc43d0909a0
SamyakMahajan <ketann682@gmail.com>
2023-07-14 16:35:11 +0530

    added few madded finalpredict

--- HttpTrigger12/finalinit.py
+++ HttpTrigger12/finalinit.py
@@ -23,7 +23,7 @@ import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    #functionname/myconfig.json
     image_url=req.params.get('image_url')
     logging.info('image_url: %s', image_url)
 
