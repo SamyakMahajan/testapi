a497be8237c0d16679afdb29705511c8fa2f5572
SamyakMahajan <ketann682@gmail.com>
2023-07-17 14:00:13 +0530

    trying to send image

--- HttpTrigger12/twilio_test.py
+++ HttpTrigger12/twilio_test.py
@@ -14,8 +14,29 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     urllib.request.urlretrieve(image_url, 'image.jpg')
 
     # Open the image file
+    #  if not req.files or 'image' not in req.files:
+    #     return func.HttpResponse(
+    #         "Please attach an image file to the request.",
+    #         status_code=400
+    #     )
     with open('image.jpg', 'rb') as image_file:
         image_data = image_file.read()
+    # Access the image file from the request
+    # file = req.files['image']
+
+
+#     # Save the file to a temporary location
+#     temp_path = os.path.join(tempfile.gettempdir(), file.filename)
+#     file.save(temp_path)
+# ###TODO: IS IT POSSIBLE TO DIRECTLY RETURN THE IMAGE FILE?
+#     # Read the image file contents
+    
+#     # Set the appropriate content type for the response
+#     content_type = 'image/jpeg'  # Modify based on your image file type
+
+#     # Return the image file as the response
+#     return func.HttpResponse(body=image_data, status_code=200, mimetype=content_type)
+    
 
     # Set the content type and return the image as the response
     headers = {
