61e65425156c8c465b39428e2e3d9e60759a51ae
SamyakMahajan <ketann682@gmail.com>
2023-07-17 16:44:25 +0530

    added try except block

--- HttpTrigger12/final_twilio_test.py
+++ HttpTrigger12/final_twilio_test.py
@@ -8,13 +8,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # Parse the request body as JSON
     req_body = req.get_json()
-
+    try:
     # Check if the message contains media (image)
-    if 'NumMedia' in req_body and int(req_body['NumMedia']) > 0:
-        # Extract the image URL
-        media_url = req_body['MediaUrl0']
-        # Do something with the image URL, such as storing it or processing it further
-        logging.info(f"Received image URL: {media_url}")
+        if 'NumMedia' in req_body and int(req_body['NumMedia']) > 0:
+            # Extract the image URL
+            media_url = req_body['MediaUrl0']
+            # Do something with the image URL, such as storing it or processing it further
+            logging.info(f"Received image URL: {media_url}")
 
-    return func.HttpResponse("OK", status_code=200)
+        return func.HttpResponse("OK", status_code=200)
+    
+    except:
+         return func.HttpResponse("Not OK", status_code=400)
 
