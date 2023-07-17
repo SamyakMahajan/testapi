a6b1178712df89b809448d1f189031f631234684
SamyakMahajan <ketann682@gmail.com>
2023-07-18 02:50:53 +0530

    -

--- HttpTrigger12/final_twilio_test.py
+++ HttpTrigger12/final_twilio_test.py
@@ -3,31 +3,24 @@ import json
 import azure.functions as func
 # from urllib.parse import parse_qs
 import urllib.parse
-import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # Parse the request body as JSON
-    req_body = req.get_body().decode('utf-8')
+    req_body = req.get_body()
     #
-    ##decoded_string = original_string.decode('utf-8')
-
     parsed_string = urllib.parse.parse_qs(req_body)
 
     json_data = dict(parsed_string)
     # json_data = dict(parsed_string)
-    # s=""
-    final_data={}
+    s=""
     for i in list(json_data):
-        # final_data[str(i)] = json_data[i]
-        s+=i
+        s+=str(i)
+        s+=" "
     # print(s)
-    
-    
     # k=json_data["SmsMessageSid"]    
-    # return func.HttpResponse(f"{final_data['MediaUrl0'][0]}", status_code=200)
     return func.HttpResponse(f"{s}", status_code=200)
+    
     # except:
     #      return func.HttpResponse("Not OK", status_code=400)
-
