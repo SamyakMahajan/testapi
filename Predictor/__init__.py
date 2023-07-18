b32e3082a5e1c14fe1292b1ede0dc08b4658d48a
SamyakMahajan <ketann682@gmail.com>
2023-07-18 14:32:30 +0530

    trying to fix something

--- Predictor/__init__.py
+++ Predictor/__init__.py
@@ -5,9 +5,9 @@ import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    req_body = req.get_json()
-    req_data = json.loads(req_body)
-    name = req_data['name']
+    req_body = dict(req.get_json())
+    # req_data = json.loads(req_body)
+    name = req_body['name']
     if not name:
         try:
             req_body = req.get_json()
