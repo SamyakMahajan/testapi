6c9a5915d3b5099cadb8f3680904ed73a297debc
SamyakMahajan <ketann682@gmail.com>
2023-07-18 09:29:24 +0530

    reverted to mediaurl getter

--- HttpTrigger12/final_twilio_test.py
+++ HttpTrigger12/final_twilio_test.py
@@ -8,19 +8,22 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # Parse the request body as JSON
-    req_body = req.get_body()
+    req_body = req.get_body().decode('utf-8')
     #
+    ##decoded_string = original_string.decode('utf-8')
+
     parsed_string = urllib.parse.parse_qs(req_body)
 
     json_data = dict(parsed_string)
     # json_data = dict(parsed_string)
-    s=""
+    # s=""
+    final_data={}
     for i in list(json_data):
-        s+=str(i)
-        s+=" "
+        final_data[str(i)] = json_data[i]
+        # s+=" "
     # print(s)
     # k=json_data["SmsMessageSid"]    
-    return func.HttpResponse(f"{s}", status_code=200)
+    return func.HttpResponse(f"{final_data['MediaUrl0'][0]}", status_code=200)
     
     # except:
     #      return func.HttpResponse("Not OK", status_code=400)
