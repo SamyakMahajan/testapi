3d8225a953c49375297035e8635d7512fa69b3ac
SamyakMahajan <ketann682@gmail.com>
2023-07-19 10:15:29 +0530

    removed the text image conditional block

--- HttpTrigger12/api_call_init.py
+++ HttpTrigger12/api_call_init.py
@@ -22,8 +22,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     for i in list(json_data):
         final_data[str(i)] = json_data[i]
         # s+=" "
-    if(int(final_data['NumMedia'][0])==0):
-        return func.HttpResponse(f"Please upload a picture", status_code=200)
+    # if(int(final_data['NumMedia'][0])==0):
+    #     return func.HttpResponse(f"Please upload a picture", status_code=200)
     # print(s)'
     # a=requests.post("https://jb-eb-d-functionapp001.azurewebsites.net/api/Predictor?code=3Lj3drbXQVpO3bPSSDyMRX_PNxD5UXir02JYkyO1TA09AzFubN7qMw==")
     # a=a.content.decode('utf-8')
@@ -34,16 +34,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # headers = {'Content-type': 'application/json'}
     # response = requests.post(url, data=json_object, headers=headers)d
     # response=response.content.decode('utf-8')
-    else:
-        media_url=final_data['MediaUrl0'][0]
-        k={"name":str(media_url)}
-        url="https://jb-eb-d-functionapp001.azurewebsites.net/api/Predictor?code=3Lj3drbXQVpO3bPSSDyMRX_PNxD5UXir02JYkyO1TA09AzFubN7qMw=="
-        json_object = json.dumps(k)
-        headers = {'Content-type': 'application/json'}
-        response = requests.post(url, data=json_object, headers=headers)
-        response=response.content.decode('utf-8')
-        logging.info(type(response))
-        return func.HttpResponse(f"Hi the response is {response}", status_code=200)
+    # else:
+    media_url=final_data['MediaUrl0'][0]
+    k={"name":str(media_url)}
+    url="https://jb-eb-d-functionapp001.azurewebsites.net/api/Predictor?code=3Lj3drbXQVpO3bPSSDyMRX_PNxD5UXir02JYkyO1TA09AzFubN7qMw=="
+    json_object = json.dumps(k)
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=json_object, headers=headers)
+    response=response.content.decode('utf-8')
+    logging.info(type(response))
+    return func.HttpResponse(f"Hi the response is {response}", status_code=200)
     
     # except:
     #      return func.HttpResponse("Not OK", status_code=400)
