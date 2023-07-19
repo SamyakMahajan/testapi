1aaa002a888584675e287bbb156bc522781e0ee8
SamyakMahajan <ketann682@gmail.com>
2023-07-19 10:10:30 +0530

    reverted to kunals file

--- Predictor/__init__.py
+++ Predictor/__init__.py
@@ -1,21 +1,10 @@
 import logging
-import azure.functions as func
 import json
+import azure.functions as func
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    req_body = req.get_body().decode('utf-8')
 
-    try:
-        req_body = req.get_json()
-        # Assuming the JSON request contains a 'message' field
-        message = req_body.get('name')
-
-        # Process the message
-        # processed_message = process_message(message)
-
-        response = {'result': message}
-        return func.HttpResponse(json.dumps(response), mimetype='application/json')
-    
-    except ValueError as e:
-        logging.error(e)
-        return func.HttpResponse('Invalid JSON', status_code=400)
+    return func.HttpResponse(f"{req_body}", status_code=200)
