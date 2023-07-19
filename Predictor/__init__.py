958122b3bf275549e06a35849f8f010e4acd891a
SamyakMahajan <ketann682@gmail.com>
2023-07-19 09:58:20 +0530

    -

--- Predictor/__init__.py
+++ Predictor/__init__.py
@@ -6,5 +6,5 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     req_body = req.get_body().decode('utf-8')
-    req_dict=json.loads(req_body)
-    return func.HttpResponse(f"{req_body}", status_code=200)
+    # req_dict=json.loads(req_body)
+    return func.HttpResponse(f"{type(req_body)}", status_code=200)
