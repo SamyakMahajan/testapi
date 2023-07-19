0df5dd475c93f99708dcfb4b297f0eb00bcbe2af
SamyakMahajan <ketann682@gmail.com>
2023-07-19 09:54:54 +0530

    worked, checking if response to dict works

--- Predictor/__init__.py
+++ Predictor/__init__.py
@@ -1,10 +1,10 @@
 import logging
-
+import json
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     req_body = req.get_body().decode('utf-8')
-
+    req_dict=json.loads(req_body)
     return func.HttpResponse(f"{req_body}", status_code=200)
