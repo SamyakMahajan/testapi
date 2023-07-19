cb4f967af206c53b15738c98721004c64ad4cb5e
SamyakMahajan <ketann682@gmail.com>
2023-07-19 10:18:31 +0530

    worked! Added The code to process json

--- Predictor/__init__.py
+++ Predictor/__init__.py
@@ -1,10 +1,18 @@
 import logging
 import json
 import azure.functions as func
+import urllib.parse
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     req_body = req.get_body().decode('utf-8')
+    parsed_string = urllib.parse.parse_qs(req_body)
 
+    json_data = dict(parsed_string)
+    # json_data = dict(parsed_string)
+    # s=""
+    final_data={}
+    for i in list(json_data):
+        final_data[str(i)] = json_data[i]
     return func.HttpResponse(f"{req_body}", status_code=200)
